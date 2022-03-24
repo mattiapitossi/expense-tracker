691058f145b6d187d5e6a248cdf584d8ebb9a751
MattiaPitossi <mattiapitossi@icloud.com>
2022-03-24 08:30:54 +0100

    Add Category field to User model

--- src/main/java/com/mattiap/expensetracker/model/User.java
+++ src/main/java/com/mattiap/expensetracker/model/User.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -23,4 +21,7 @@ public class User {
     private String name;
 
     private String email;
+
+    @OneToMany
+    private Set<Category> category;
 }
