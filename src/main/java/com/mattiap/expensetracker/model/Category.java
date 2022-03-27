2a4aa8dcf2bd516875734cbefc024bb5c0cb14bf
MattiaPitossi <mattiapitossi@icloud.com>
2022-03-27 12:56:00 +0200

    Remove User field from Category entity

--- src/main/java/com/mattiap/expensetracker/model/Category.java
+++ src/main/java/com/mattiap/expensetracker/model/Category.java
@@ -3,7 +3,10 @@ package com.mattiap.expensetracker.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @NoArgsConstructor
@@ -14,8 +17,7 @@ public class Category {
     @Id
     private Long id;
 
+    @NotNull
     private String name;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    private User user;
 }
