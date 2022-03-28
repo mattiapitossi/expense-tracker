f81dfd909b176d13a2de02bff0d285d941141eac
MattiaPitossi <mattiapitossi@icloud.com>
2022-03-28 20:38:30 +0200

    Add home component

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import AppNav from './AppNav';
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (<AppNav/>);
+    }
+}
+ 
+export default Home;
