18a13e05aca215399ec8380d1347367b5125edc1
MattiaPitossi <mattiapitossi@icloud.com>
2022-03-28 20:38:15 +0200

    Update Category component

--- frontend/src/Category.js
+++ frontend/src/Category.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Category extends Component {
-    state = {
-        isLoading : true,
-        Categories: []
-    }
+  state = {
+    isLoading: true,
+    Categories: [],
+  };
 
-    async componentDidMount() {
-        const response = await fetch('/api/category')
-    }
+  async componentDidMount() {
+    const response = await fetch("/api/category");
+    const body = await response.json();
+    this.setState({ Categories: body, isLoading: false });
+  }
 
-    render() { 
-        return ();
-    }
+  render() {
+    const { Categories, isLoading } = this.state;
+    if (isLoading) return <div>Loading...</div>;
+
+    return (
+      <div>
+        <h2>Categories</h2>
+        {Categories.map((category) => (
+          <div id="category.id">{category.name}</div>
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Category;
