82896c744f3bc0ecd10cfc10a1e4780bf861d4ba
MattiaPitossi <mattiapitossi@icloud.com>
2022-03-28 14:13:11 +0200

    Update index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Category from './Category';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Category />
   </React.StrictMode>,
   document.getElementById('root')
 );
