92b8be4ee3215ea052a6e322a2621c216c7b0362
MattiaPitossi <mattiapitossi@icloud.com>
2022-04-20 13:30:33 +0200

    Add Expenses component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Category from "./Category";
 import Home from "./Home";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Expenses from "./Expenses";
 
 class App extends Component {
   state = {};
@@ -11,6 +12,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact={true} component={Home} />
           <Route path="/categories" exact={true} component={Category} />
+          <Route path="/expenses" exact={true} component={Expenses} />
         </Switch>
       </Router>
     );
---
+++ frontend/src/Expenses.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import AppNav from "./AppNav";
+
+class Expenses extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <AppNav />
+        <h2>Expenses</h2>
+      </div>
+    );
+  }
+}
+
+export default Expenses;
