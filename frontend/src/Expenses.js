d5c1b9e6621b872f01ccd90b8cb9943af41b50f3
simo2898 <70075823+simo2898@users.noreply.github.com>
2022-05-21 19:38:09 +0200

    Add hibernate properties to application.properties

--- frontend/src/Expenses.js
+++ frontend/src/Expenses.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container, Form, FormGroup } from "reactstrap";
 import AppNav from "./AppNav";
 
 class Expenses extends Component {
@@ -7,7 +8,36 @@ class Expenses extends Component {
     return (
       <div>
         <AppNav />
+        <Container>
         <h2>Expenses</h2>
+          <Form>
+            <FormGroup>
+              <label for = "title">Title</label>
+              <input type = "text" name = "title" id = "title" onChange={this.handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <label for = "category">Category</label>
+              <input type = "text" name = "category" id = "category" onChange={this.handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <label for = "date">Expense Date</label>
+              <input type = "text" name = "date" id = "date" onChange={this.handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <label for = "location">Location</label>
+              <input type = "text" name = "location" id = "location" onChange={this.handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <Button color = "primary" type = "submit">Save</Button>{' '}
+              <Button color = "secondary" tag = {Link} to = "/categories">Cancel</Button>{' '}
+            </FormGroup>
+
+          </Form>
+        </Container>
       </div>
     );
   }
