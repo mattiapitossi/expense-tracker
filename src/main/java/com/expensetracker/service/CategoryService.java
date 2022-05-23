90a46c1f3827f4c660e5e5fdcbc22cea3efb9035
Mattia <mattiapitossi@icloud.com>
2022-05-23 12:00:27 +0200

    Add Category Service

---
+++ src/main/java/com/expensetracker/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.expensetracker.service;
+
+import com.expensetracker.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+}
---
+++ src/main/java/com/expensetracker/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.expensetracker.service.impl;
+
+import com.expensetracker.model.Category;
+import com.expensetracker.repository.CategoryRepository;
+import com.expensetracker.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
