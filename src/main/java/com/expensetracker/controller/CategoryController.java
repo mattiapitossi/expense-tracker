924c340cb6f1dedebb3908fe7219162decd167ac
Mattia <mattiapitossi@icloud.com>
2022-05-23 12:02:38 +0200

    Remove no longer used Class import

--- src/main/java/com/expensetracker/controller/CategoryController.java
+++ src/main/java/com/expensetracker/controller/CategoryController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
