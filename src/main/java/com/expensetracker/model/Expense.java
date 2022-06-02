fab453312ea2f69ef00276a4a0330dbb08182f3d
GinoGreen <luigiverde97@gmail.com>
2022-06-02 23:29:43 +0200

    LocalDate expence_date

--- src/main/java/com/expensetracker/model/Expense.java
+++ src/main/java/com/expensetracker/model/Expense.java
@@ -2,12 +2,15 @@ package com.expensetracker.model;
 
 import com.expensetracker.type_enum.TypePayment;
 import com.expensetracker.type_enum.TypeTransaction;
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -24,7 +27,7 @@ public class Expense {
 
     @JsonProperty("expense_date")
     @Column(name = "expense_date")
-    private LocalDateTime expenseDate;
+    private LocalDate expenseDate;
 
     @Column(name = "title")
     private String title;
@@ -53,4 +56,12 @@ public class Expense {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @JsonSetter
+    public void setCategory(String category) {
+        this.category = new Category();
+        this.category.setName(category);
+    }
+
+    public void setCategory(Category category) { this.category = category; }
 }
