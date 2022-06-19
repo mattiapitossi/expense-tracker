8ea87971f3ee118295bf8ada4d5562cdc831f778
Mattia <mattiapitossi@icloud.com>
2022-06-19 23:19:17 +0200

    Extract CategoryService interface

--- src/main/java/com/expensetracker/service/CategoryService.java
+++ src/main/java/com/expensetracker/service/CategoryService.java
@@ -3,9 +3,18 @@ package com.expensetracker.service;
 import com.expensetracker.model.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryService {
     List<Category> getAllCategories();
 
+    Optional<Category> findCategoryById(Integer id);
 
+    Category createNewCategory(Category category);
+
+    Category modifyCategory(Category category);
+
+    void deleteById(Integer categoryId);
+
+    Category findByName(String name);
 }
